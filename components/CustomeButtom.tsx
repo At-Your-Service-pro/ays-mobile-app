38566f009d9a1cba5fdb8c493ed82e18c44a1242
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-10-21 06:34:54 +0000

    restructure folder

---
+++ components/CustomeButtom.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomeButtom = () => {
+  return (
+    <View>
+      <Text>CustomeButtom</Text>
+    </View>
+  )
+}
+
+export default CustomeButtom
