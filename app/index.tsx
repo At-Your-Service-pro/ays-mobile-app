685e14e3bebcee6d37c0c92f6a2eb234ca6d38b3
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-11-06 10:22:17 +0000

    Update mobile-app

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 import React from 'react'
 import "../global.css"
 
+const { width, height } = Dimensions.get('window');
+
 const index = () => {
+
+  const onboarding = [
+    {
+      id: 1,
+      title: 'Get variety of professional sevrices',
+      description: 'Get differet professional services anywhere you are',
+      image: require('../assets/images/1.png'),
+    },
+    {
+      id: 2,
+      title: 'Connect with professionals',
+      description: 'Connect with highly skilled and professional service providers',
+      image: require('../assets/images/2.png'),
+    },
+    {
+      id: 3,
+      title: 'At affordable prices',
+      description: 'Get professionals for cheap cost',
+      image: require('../assets/images/3.png'),
+    }
+
+  ];
+
   return (
     <View style={styles.container}>
       <Text>index</Text>
