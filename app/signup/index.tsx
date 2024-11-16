a4e245afcc788f5f9035f06019803a0ea7a7f300
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-11-16 06:00:07 +0000

    Update mobile-app

--- app/signup/index.tsx
+++ app/signup/index.tsx
@@ -6,20 +6,22 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
   Dimensions
-} from 'react-native'
-import React,{useState} from 'react'
+} from 'react-native';
+import React, { useState } from 'react';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { router,Link } from 'expo-router';
+import { router } from 'expo-router';
 import Entypo from '@expo/vector-icons/Entypo';
 import Fontisto from '@expo/vector-icons/Fontisto';
 import CustomeButtom from '@/components/CustomeButtom';
 
-const {height} = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 
 const index = () => {
-  const [showPassword,setShowPassword] = useState(false);
-  const [formData,setFormData] = useState({
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
     firstname: '',
     lastname: '',
     email: '',
@@ -27,168 +29,112 @@ const index = () => {
     phonenumber: ''
   });
 
-  const handleChange = (event: any) => {
+  const handleChange = (id: string, value: string) => {
     setFormData({
-       ...formData,
-        [event.target.id]: event.target.value
-    })
-  }
+      ...formData,
+      [id]: value
+    });
+  };
+
   return (
-    <SafeAreaView 
-      style={styles.container}
-    >
-      <ScrollView
-        showsVerticalScrollIndicator={false} 
-        style={{
-          height: '200%',
-          paddingBottom: 500
-        }}
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Adjust for platform
+        style={{ flex: 1 }}
       >
-        <View
-          style={styles.header}
-        >
-          <AntDesign name="left" size={24} color="black" onPress={() => router.back()}/>
-          <View>
-            <Text style={styles.headerText}> Create Account </Text>
-          </View>
-        </View>
-        <View
-          style={{
-            width: '90%',
-            marginHorizontal: 'auto'
-          }}
-        >
-          <View
-            style={{
-              marginTop: 20
-            }}
-          >
-              <Text
-                style={styles.loginTextHeader}
-              > Firstname </Text>
-              <View 
-                style={styles.loginContainerText}
-              >
-                <AntDesign name="user" size={24} color="black" />
-                <TextInput
-                    id='firstname'
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{ height: '300%' }}>
+            <View style={styles.header}>
+              <AntDesign name="left" size={24} color="black" onPress={() => router.back()} />
+              <View>
+                <Text style={styles.headerText}>Create Account</Text>
+              </View>
+            </View>
+            <View style={{ width: '90%', marginHorizontal: 'auto' }}>
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.loginTextHeader}>Firstname</Text>
+                <View style={styles.loginContainerText}>
+                  <AntDesign name="user" size={24} color="black" />
+                  <TextInput
                     placeholder=""
                     keyboardType="default"
                     style={styles.loginInput}
-                    onChange={handleChange}
+                    onChangeText={(text) => handleChange('firstname', text)}
                   />
+                </View>
               </View>
-            </View>
-            <View
-            style={{
-              marginTop: 20
-            }}
-          >
-              <Text
-                style={styles.loginTextHeader}
-              > Lastname </Text>
-              <View 
-                style={styles.loginContainerText}
-              >
-                <AntDesign name="user" size={24} color="black" />
-                <TextInput
-                    id='lastname'
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.loginTextHeader}>Lastname</Text>
+                <View style={styles.loginContainerText}>
+                  <AntDesign name="user" size={24} color="black" />
+                  <TextInput
                     placeholder=""
                     keyboardType="default"
                     style={styles.loginInput}
-                    onChange={handleChange}
+                    onChangeText={(text) => handleChange('lastname', text)}
                   />
+                </View>
               </View>
-            </View>
-            <View
-              style={{
-                marginTop: 20
-              }}
-            >
-              <Text
-                style={styles.loginTextHeader}
-              > Email </Text>
-              <View 
-                style={styles.loginContainerText}
-              >
-                <Fontisto name="email" size={24} color="black" />
-                <TextInput
-                    id='email'
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.loginTextHeader}>Email</Text>
+                <View style={styles.loginContainerText}>
+                  <Fontisto name="email" size={24} color="black" />
+                  <TextInput
                     placeholder=""
                     keyboardType="email-address"
                     style={styles.loginInput}
-                    onChange={handleChange}
+                    onChangeText={(text) => handleChange('email', text)}
                   />
+                </View>
               </View>
-            </View>
-            <View
-              style={{
-                marginTop: 20
-              }}
-            >
-             <Text
-              style={styles.loginTextHeader}
-             > Password </Text>
-             <View 
-              style={styles.loginContainerText}
-             >
-              <TextInput
-                  id='password'
-                  placeholder="Enter Password"
-                  keyboardType="default"
-                  style={styles.loginInput}
-                  secureTextEntry={!showPassword}
-                  onChange={handleChange}
-                />
-                <TouchableOpacity
-                  onPress={() => setShowPassword(!showPassword)}
-                >
-                  <Entypo 
-                    name={showPassword ? 'eye' : 'eye-with-line'} 
-                    size={20} 
-                    color="black" 
-                  />
-                </TouchableOpacity>
-             </View>
-          </View>
-          <View
-            style={{
-              marginTop: 20
-            }}
-          >
-              <Text
-                style={styles.loginTextHeader}
-              > Phone number </Text>
-              <View 
-                style={styles.loginContainerText}
-              >
-                <TextInput
-                    id='phonenumber'
-                    placeholder="e.g 0245679625"
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.loginTextHeader}>Password</Text>
+                <View style={styles.loginContainerText}>
+                  <TextInput
+                    placeholder="Enter Password"
                     keyboardType="default"
                     style={styles.loginInput}
-                    onChange={handleChange}
+                    secureTextEntry={!showPassword}
+                    onChangeText={(text) => handleChange('password', text)}
                   />
+                  <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                    <Entypo 
+                      name={showPassword ? 'eye' : 'eye-with-line'} 
+                      size={20} 
+                      color="black" 
+                    />
+                  </TouchableOpacity>
+                </View>
               </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.loginTextHeader}>Phone number</Text>
+                <View style={styles.loginContainerText}>
+                  <TextInput
+                    placeholder="e.g 0245679625"
+                    keyboardType="phone-pad"
+                    style={styles.loginInput}
+                    onChangeText={(text) => handleChange('phonenumber', text)}
+                  />
+                </View>
+              </View>
+              <View style={styles.privacy}>
+                <Text>
+                  By tapping "Continue" you agree to our <Text style={{ color: '#1AACD5' }}>Terms of Use</Text> and <Text style={{ color: '#1AACD5' }}>Privacy Policy</Text>
+                </Text>
+              </View>
+              <CustomeButtom 
+                title="Continue"
+                onPress={() => router.push('/otp')}
+              />
             </View>
-            <View
-              style={styles.privacy}
-            >
-               <Text>
-                By tapping "Continue" you agree to our <Text style={{color: '#1AACD5'}}>Terms of Use</Text> and <Text style={{color: '#1AACD5'}}>Privacy Policy</Text>
-               </Text>
-            </View>
-           <CustomeButtom 
-            title='Continue'
-            onPress={() => router.push('/otp')}
-           />
-        </View>
-      </ScrollView>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default index
+export default index;
 
 const styles = StyleSheet.create({
   container: {
@@ -198,9 +144,8 @@ const styles = StyleSheet.create({
     marginTop: '10%',
   },
   header: {
-    display: 'flex',
     flexDirection: 'row',
-    justifyContent:'space-between',
+    justifyContent: 'space-between',
     alignItems: 'center',
     paddingVertical: 16,
     paddingHorizontal: 16,
@@ -208,21 +153,20 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontSize: 25,
-    fontWeight: 'semibold',
-    textAlign: 'center'
+    fontWeight: '600',
+    textAlign: 'center',
   },
   loginContainerText: {
-    display: 'flex',
     flexDirection: 'row',
     backgroundColor: '#E1E1E1',
     borderRadius: 7,
     alignItems: 'center',
     paddingLeft: 15,
-    marginTop: 10
+    marginTop: 10,
   },
   loginTextHeader: {
     fontSize: 20,
-    fontWeight: 'regular'
+    fontWeight: '400',
   },
   loginInput: {
     padding: 7,
@@ -230,30 +174,13 @@ const styles = StyleSheet.create({
     borderRadius: 7,
     marginTop: 10,
     fontSize: 18,
-    width: '85%'
+    width: '85%',
   },
   privacy: {
-    display: 'flex',
-    flexDirection: 'column',
     alignItems: 'center',
     marginTop: 20,
     width: '85%',
     marginHorizontal: 'auto',
-    marginBottom: 10
+    marginBottom: 10,
   },
-  link: {
-    backgroundColor: '#1AACD5',
-    padding: 10,
-    borderRadius: 10,
-    margin: 10,
-    alignSelf: 'center',
-    width: '100%',
-    height: height / 13
-  },
-  text: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'medium',
-    textAlign: 'center',
-  }
-})
+});
