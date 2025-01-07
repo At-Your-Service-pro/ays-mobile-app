e1fd1cf98139435429a1d4d4c69b488d2a32e1df
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-01-07 16:48:41 +0000

    updated dashboard ui

--- app/(tabs)/dashboard/index.tsx
+++ app/(tabs)/dashboard/index.tsx
@@ -6,7 +6,9 @@ import {
   SafeAreaView,
   KeyboardAvoidingView,
   Platform,
-  Image 
+  Image,
+  FlatList,
+  TouchableOpacity 
 } from 'react-native';
 import React,{useEffect,useState} from 'react';
 import { useLocalSearchParams } from 'expo-router';
@@ -43,6 +45,78 @@ const index = () => {
     {
       id: '3',
       title: 'Capenters',
+    },
+    {
+      id: '4',
+      title: 'Plumbing',
+    },
+    {
+      id: '5',
+      title: 'Floor fitters',
+    },
+    {
+      id: '6',
+      title: 'Capenters',
+    }
+  ]
+
+  const services = [
+    {
+      id: '1',
+      title: 'Plumbing Installation',
+      category: 'Plumbing',
+      loaction: 'Spintex',
+      image: require('../../../assets/images/3.png'),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+      phonenumber: '0245861319',
+      sub_services: [
+        {
+          title: 'leak repair',
+          price: '$100'
+        },
+        {
+          title: 'leak repair',
+          price: '$100'
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Plumbing Installation',
+      category: 'Plumbing',
+      loaction: 'Spintex',
+      image: require('../../../assets/images/3.png'),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+      phonenumber: '0245861319',
+      sub_services: [
+        {
+          title: 'leak repair',
+          price: '$100'
+        },
+        {
+          title: 'leak repair',
+          price: '$100'
+        }
+      ]
+    },
+    {
+      id: '3',
+      title: 'Plumbing Installation',
+      category: 'Plumbing',
+      loaction: 'Spintex',
+      image: require('../../../assets/images/3.png'),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+      phonenumber: '0245861319',
+      sub_services: [
+        {
+          title: 'leak repair',
+          price: '$100'
+        },
+        {
+          title: 'leak repair',
+          price: '$100'
+        }
+      ]
     }
   ]
 
@@ -76,24 +150,50 @@ const index = () => {
 		  <View
 		  	style={styles.categories}
 		  >
-			<Text
-				style={{
-					backgroundColor: '#1AACD5',
-					color: '#FFFFFF',
-					padding: 10,
-					fontSize: 17,
-					borderRadius: 7,
-					width: '35%',
-					textAlign: 'center'
-				}}
-			> Categories</Text>
-			<Text 
-				style={{
-					color: '#1AACD5',
-					fontSize: 16
-				}}> All </Text>
+        <Text
+          style={{
+            backgroundColor: '#1AACD5',
+            color: '#FFFFFF',
+            padding: 10,
+            fontSize: 17,
+            borderRadius: 7,
+            width: '35%',
+            textAlign: 'center'
+          }}
+        > Categories</Text>
+        <Text 
+          style={{
+            color: '#1AACD5',
+            fontSize: 16
+          }}> All </Text>
 		  </View>
-        </ScrollView>
+      <View style={styles.categoryContainer}>
+        <FlatList
+          data={_categories}
+          // keyExtractor={(item) => item.id}
+          horizontal // Enables horizontal scrolling
+          showsHorizontalScrollIndicator={false} // Hides the scrollbar
+          renderItem={({ item }) => (
+            <TouchableOpacity  style={styles.subcategory}>
+              <Text style={{textAlign: 'center'}}>{item.title}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+      <View style={styles.popularContainer}>
+          <View>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: 'semibold'
+              }}
+            > Popular services </Text>
+          </View>
+          <View>
+
+          </View>
+      </View>
+      </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
   )
@@ -133,10 +233,25 @@ const styles = StyleSheet.create({
 	fontStyle: 'italic'
   },
   categories: {
-    marginTop: '5%',
+    marginTop: '7%',
 	  display: 'flex',
 	  flexDirection: 'row',
     justifyContent:'space-between',
     alignItems: 'center'
-  }  
+  },
+  categoryContainer: {
+    marginTop: '4%',
+    flex: 1,
+  },
+  subcategory: {
+    backgroundColor: '#E4E4E4',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  popularContainer: {
+    marginTop: '10%'
+  }
+
 })
